4e734a67b631df6b2eeaf6eb36d268415a02abd3
Aliaksandr Naronski <Aliaksandr.Naronski@itechart-group.com>
2022-02-10 09:20:11 +0300

    comment commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -12,7 +12,73 @@
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
 //
+/*import {amount, currency, leverage, value} from "../locators";
+import Chance from 'chance';
 
+Cypress.Commands.add('chooseRandomLeveragesFSA', () => {
+    cy.fixture("tradeAccs.json").as('tradeAccData').then((tradeAccData) => {
+        let randomLeverageFSA = chance.pickone(tradeAccData.leveragesFSA.map(function (e) {
+            return e.leverage;
+        }))
+        cy.get(`${leverage}`).click()
+        cy.get(`${value}`).contains(randomLeverageFSA).click()
+    })
+})
+
+Cypress.Commands.add('chooseRandomLeveragesCySEC', () => {
+    cy.fixture("tradeAccs.json").as('tradeAccData').then((tradeAccData) => {
+        let randomLeverageCySEC = chance.pickone(tradeAccData.leveragesCySEC.map(function (e) {
+            return e.leverage;
+        }))
+        cy.get(`${leverage}`).click()
+        cy.get(`${value}`).contains(randomLeverageCySEC).click()
+    })
+})
+
+Cypress.Commands.add('chooseRandomLeveragesProfCySEC', () => {
+    cy.fixture("tradeAccs.json").as('tradeAccData').then((tradeAccData) => {
+        let randomLeverageProfCySEC = chance.pickone(tradeAccData.leveragesProfCySEC.map(function (e) {
+            return e.leverage;
+        }))
+        cy.get(`${leverage}`).click()
+        cy.get(`${value}`).contains(randomLeverageProfCySEC).click()
+    })
+})
+
+Cypress.Commands.add('chooseRandomCurrency', () => {
+    cy.fixture("tradeAccs.json").as('tradeAccData').then((tradeAccData) => {
+        let randomCurrency = chance.pickone(tradeAccData.currency.map(function (e) {
+            return e.shortName;
+        }))
+        cy.get(`${currency}`).click()
+        cy.get(`${value}`).contains(randomCurrency).click()
+    })
+})
+
+Cypress.Commands.add('chooseRandomAmount', () => {
+    cy.fixture("tradeAccs.json").as('tradeAccData').then((tradeAccData) => {
+        let randomAmount = chance.pickone(tradeAccData.amounts.map(function (e) {
+            return e.amount;
+        }))
+        cy.get(`${amount}`).click()
+        cy.get(`${value}`).contains(randomAmount).click()
+    })
+})
+
+Cypress.Commands.add('chooseRandomAmountAndCurrency', () => {
+    cy.fixture("tradeAccs.json").as('tradeAccData').then((tradeAccData) => {
+        let randomAmount = chance.pickone(tradeAccData.amounts.map(function (e) {
+            return e.amount;
+        }))
+        let randomCurrency = chance.pickone(tradeAccData.currency.map(function (e) {
+            return e.shortName;
+        }))
+        cy.get(`${currency}`).click()
+        cy.get(`${value}`).contains(randomCurrency).click()
+        cy.get(`${amount}`).click()
+        cy.get(`${value}`).contains(randomAmount+" "+randomCurrency).click()
+    })
+})*/
 
 
 //
