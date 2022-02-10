46d3140d242e0d9b0746cc6555f99cb6b5eaa37c
Aliaksandr Naronski <Aliaksandr.Naronski@itechart-group.com>
2022-02-10 09:32:52 +0300

    rename enterWrongLoginData

--- cypress/page-objects/loginPage.js
+++ cypress/page-objects/loginPage.js
@@ -9,19 +9,19 @@ import {
 
 class LoginPage {
     get searchButtonSignIn(){
-        return cy.get(`${buttonSignIn}`)
+        return cy.get('.css-1m5rymt.e1w14daj6')
     }
     get searchFSAIcon(){
-        return cy.get(`${fsaIcon}`)
+        return cy.get('[src="https://images.prismic.io/axiance/ODY5YTcwNTAtYjg2Ny00MjMxLWJhNTktNTAxOWYwY2RiN2Yx_68936e02-afac-41cc-a78d-8f15f3bf20ac_group4163.png?ixlib=gatsbyFP&q=50&auto=compress%2Cformat&rect=0%2C0%2C92%2C92&w=92&h=92"]')
     }
     get searchCySECIcon(){
-        return cy.get(`${cysecIcon}`)
+        return cy.get('[src="https://images.prismic.io/axiance/ODY5YTcwNTAtYjg2Ny00MjMxLWJhNTktNTAxOWYwY2RiN2Yx_68936e02-afac-41cc-a78d-8f15f3bf20ac_group4163.png?ixlib=gatsbyFP&q=50&auto=compress%2Cformat&rect=0%2C0%2C92%2C92&w=92&h=92"]')
     }
     get searchEmailInput(){
-        return cy.get(`${inputEmail}`)
+        return cy.get('#email')
     }
     get searchPasswordInput(){
-        return cy.get(`${inputPassword}`)
+        return cy.get('#password')
     }
 
     open(){
@@ -37,21 +37,21 @@ class LoginPage {
         this.searchCySECIcon.should("be.visible").click();
     }
     openNewUrlFSA(){
-        cy.visit(`${newUrlFSA}`, {failOnStatusCode: false})
+        cy.visit('https://client.axiance.com/int/sign-in?', {failOnStatusCode: false})
     }
     openNewUrlCySEC(){
-        cy.visit(`${newUrlCySEC}`, {failOnStatusCode: false})
+        cy.visit('https://client.axianceeu.com/sign-in?', {failOnStatusCode: false})
     }
     login(email,password){
         this.searchEmailInput.type(`${email}{enter}`);
         this.searchPasswordInput.type(`${password}{enter}`);
     }
     checkSideBarExist(){
-        return cy.get(`${sideBar}`).should("be.visible").should('exist')
+        return cy.get('.styles_sidebar__8-r_L').should("be.visible")
     }
     //todo переименовать enterWrongLoginData
-    enterWrongLoginData(){
-        return cy.get(`${loginData}`).should("be.visible").should('exist')
+    checkExistenceOfErrorMessage(){
+        return cy.get('.style_default__1exWx.style_s12__2GuCl').should("be.visible")
     }
 }
 export default new LoginPage()
